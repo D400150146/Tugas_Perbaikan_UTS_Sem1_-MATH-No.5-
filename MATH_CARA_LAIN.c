2ca37bc2cb0d4b4a2946504fb702fc9b0f32ed5a
D400150146 <nfahmifauz@gmail.com>
2015-11-17 14:18:37 +0700

    menambah file baru'

---
+++ MATH_CARA_LAIN.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+float fungsiku(x, m){
+	float y ;
+	y = sqrt(5 * pow(x, 3) - pow (x, (m-1)) + 12);
+	return y;
+}
+
+int main(){
+	int a, b;
+	printf("Masukkan Nilai a = ");scanf("%i", &a);
+	printf("Masukkan Nilai b = ");scanf("%i", &b);
+    float y = 0;
+	y = fungsiku(a, b);
+	printf("-------------------------------------\n");
+	printf("x=%d, m=%d, maka hasilnya y=%f \n",a,b,y);
+	printf ("Nama  : Nur Fahmi Fauziati W.\n");
+	printf ("NIM   : D400150146\n");
+	printf ("Kelas : E\n");
+	return 0;
+}
